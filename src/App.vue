59cf0576656859b53acb9cdeac8de47cf02b08f1
Rafael Gonçalves <rafagpoliveira@gmail.com>
2020-08-13 11:14:25 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './main/Calculator'
+
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+  @font-face {
+    font-family: "RobotoMono" ;
+    src: url("./fonts/RobotoMono-Light.ttf");
+  }
+
+  * {
+    font-family: "RobotoMono", monospace;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41,46,73));
+  }
+</style>
